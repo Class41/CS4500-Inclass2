b52f03e30b38f3220a6d9c257b4009210db11e7f
vontv <vontv@es107m06.dhcp.umsl.edu>
2019-09-17 13:32:02 -0500

    added boilerplate code

---
+++ main.py
@@ -0,0 +1,41 @@
+
+def init():
+    while True:
+        doGUI()
+        command = checkCommand()
+        if command == 0:
+            readAndPush()
+        else if command == 1:
+            readAndPop()
+        else if command == 2:
+            displayTop()
+        else if command == 3:
+            exitApp()
+
+    
+    
+def readAndPush():
+
+
+
+
+
+def readAndPop():
+
+
+
+
+
+def displayTop()):
+
+
+
+
+
+
+def exitApp():
+
+
+
+if __name__ == "__main__":
+    init()
