c182931c29c9a6b458f0e60c27866ecf7b374ebc
vontv <vontv@es107m06.dhcp.umsl.edu>
2019-09-17 13:43:33 -0500

    added gui output

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 def init():
     stack = []
-
+    nextPrint = ""
     while True:
-        doGUI()
+        doGUI(nextPrint)
+        nextPrint = ""
         command = checkCommand()
         if command == 0:
-            readAndPush(stack)
+            nextPrint = readAndPush(stack)
         elif command == 1:
-            readAndPop(stack)
+            nextPrint = readAndPop(stack)
         elif command == 2:
-            displayTop(stack)
+            nextPrint = displayTop(stack)
         elif command == 3:
             exitApp()
 
@@ -35,6 +36,12 @@ def displayTop(stack):
 
 def exitApp():
 
+
+
+
+def doGUI(nextPrint):
+    guiOut = "/** Choose an action by number **\\\n\t0 -> Push to stack\n\t1 -> Pop to stack\n\t2 -> See top of stack\n\t3 -> Exit\n" + nextPrint
+    print(guiOut)
 
 
 if __name__ == "__main__":
