17bbd63d753af999fc4ee35051cf87fe4cf8a804
vontv <vontv@es107m06.dhcp.umsl.edu>
2019-09-19 13:07:11 -0500

    switched to global

--- main.py
+++ main.py
@@ -1,62 +1,53 @@
+stack = []
+
+
 def init():
-    stack = []
     nextPrint = ""
     while True:
         doGUI(nextPrint)
         nextPrint = ""
         command = checkCommand()
+
         if command == 0:
-            nextPrint = readAndPush(stack)
+            nextPrint = readAndPush()
         elif command == 1:
-            nextPrint = readAndPop(stack)
+            nextPrint = readAndPop()
         elif command == 2:
-            nextPrint = displayTop(stack)
+            nextPrint = displayTop()
         elif command == 3:
             exitApp()
 
-    
-    
-#Debugging
-stack.append("C")
-stack.append("A")
+# appending whatever character
 
-#appending whatever character
-def readAndPush(stack, c):
+
+def readAndPush():
+    global stack
     stack.append(c)
     return stack
 
-#popping off the last of the list
-def readAndPop(stack):
+# popping off the last of the list
+
+
+def readAndPop():
+    global stack
     stack.pop()
     return stack
 
-#Display last of the list
-def displayTop(stack):
+# Display last of the list
+
+
+def displayTop():
+    global stack
     return stack[-1]
 
 
-
-
-
 def exitApp():
-    sys.exit()
-
+    exit()
 
 
 def doGUI(nextPrint):
     guiOut = "/** Choose an action by number **\\\n\t0 -> Push to stack\n\t1 -> Pop to stack\n\t2 -> See top of stack\n\t3 -> Exit\n" + nextPrint
     print(guiOut)
-
-
-#Testing functions
-readAndPush(stack, "T")
-print(stack)
-
-stack = readAndPop(stack)
-print(stack)
-
-print(displayTop(stack))
-
 
 
 if __name__ == "__main__":
