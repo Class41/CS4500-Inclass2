7b8c6e7c15ce18c01eaa1995d6795cceed8eea3b
vontv <vontv@es107m06.dhcp.umsl.edu>
2019-09-19 13:34:35 -0500

    updated main loop

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
 stack = []
 
 def init():
+    global stack
     nextPrint = ""
     result = [True, "OK"]
     
     while True:
         doGUI(nextPrint)
         command = checkCommand()
-        if command == 0:
-            result = readAndPush()
-        elif command == 1:
-            result = readAndPop()
-        elif command == 2:
-            result = displayTop()
-        elif command == 3:
-            exitApp()
+        nextPrint = ""
         
+        if command != False:
+            if command == 0:
+                result = readAndPush()
+            elif command == 1:
+                result = readAndPop()
+            elif command == 2:
+                result = displayTop()
+            elif command == 3:
+                exitApp()
+            
+            nextPrint = result[1]
+            
+            if result[0]:
+                print(str(stack))
         
+def checkCommand():
+    userSelection = input()
+    
+    try:
+        userSelection = int(userSelection)
+        return userSelection
+    except Exception as e:
+        print("Unable to convert input to acceptable menu command.")
+        return False
 
 # appending whatever character
 def readAndPush():
@@ -46,7 +63,7 @@ def exitApp():
 
 
 def doGUI(nextPrint):
-    guiOut = "/** Choose an action by number **\\\n\t0 -> Push to stack\n\t1 -> Pop to stack\n\t2 -> See top of stack\n\t3 -> Exit\n" + nextPrint
+    guiOut = "/** Choose an action by number **\\\n\t0 -> Push to stack\n\t1 -> Pop to stack\n\t2 -> See top of stack\n\t3 -> Exit\n\n\n" + nextPrint
     print(guiOut)
 
 
